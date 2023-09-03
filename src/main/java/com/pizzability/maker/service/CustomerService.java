d5490cb91f85a9e8cf63384bec3d29a2c269a755
DavidGMont <davidgarmo@outlook.com>
2023-09-03 18:06:45 -0500

    📱 Método con JPQL creado e implementado

---
+++ src/main/java/com/pizzability/maker/service/CustomerService.java
@@ -0,0 +1,20 @@
+package com.pizzability.maker.service;
+
+import com.pizzability.maker.persistence.entity.CustomerEntity;
+import com.pizzability.maker.persistence.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public CustomerEntity findByPhone(String phone) {
+        return this.customerRepository.findByPhone(phone);
+    }
+}
