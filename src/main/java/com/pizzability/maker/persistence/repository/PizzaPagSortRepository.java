7a2793ce1dde0538c3570e3eb97ab68e644eac64
DavidGMont <davidgarmo@outlook.com>
2023-09-03 17:11:50 -0500

    🗃 PagingAndSorting implementado en el método getAvailable de PizzaService

--- src/main/java/com/pizzability/maker/persistence/repository/PizzaPagSortRepository.java
+++ src/main/java/com/pizzability/maker/persistence/repository/PizzaPagSortRepository.java
@@ -1,7 +1,10 @@
 package com.pizzability.maker.persistence.repository;
 
 import com.pizzability.maker.persistence.entity.PizzaEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.ListPagingAndSortingRepository;
 
 public interface PizzaPagSortRepository extends ListPagingAndSortingRepository<PizzaEntity, Integer> {
+    Page<PizzaEntity> findByAvailableTrue(Pageable pageable);
 }
