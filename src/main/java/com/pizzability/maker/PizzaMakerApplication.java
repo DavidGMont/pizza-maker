54449c808dc2da026397dad7b3b3742cc8fb02dd
DavidGMont <davidgarmo@outlook.com>
2023-09-09 23:18:00 -0500

    🕵️‍♀️ Auditable añadido a PizzaEntity

--- src/main/java/com/pizzability/maker/PizzaMakerApplication.java
+++ src/main/java/com/pizzability/maker/PizzaMakerApplication.java
@@ -3,10 +3,12 @@ package com.pizzability.maker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableJpaAuditing
 @Slf4j
 public class PizzaMakerApplication {
     public static void main(String[] args) {
---
+++ src/main/java/com/pizzability/maker/persistence/entity/AuditableEntity.java
@@ -0,0 +1,19 @@
+package com.pizzability.maker.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public class AuditableEntity {
+    @Column(name = "created_date")
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @Column(name = "last_modified_date")
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+}
