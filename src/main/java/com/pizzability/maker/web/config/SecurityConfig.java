c1d42bcbdb67c2e4c155bcdf112ab23dcbe6a4a1
DavidGMont <davidgarmo@outlook.com>
2023-10-05 11:12:47 -0500

    🕶 CSRF desactivado por la conversión a Stateless

--- src/main/java/com/pizzability/maker/web/config/SecurityConfig.java
+++ src/main/java/com/pizzability/maker/web/config/SecurityConfig.java
@@ -4,14 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.
-                authorizeHttpRequests(
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
                         matcherRegistry -> matcherRegistry
                                 .anyRequest()
                                 .authenticated())
