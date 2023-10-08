ec81ea3ada29c5a3c7023a304a7b458dd58542ab
DavidGMont <davidgarmo@outlook.com>
2023-10-08 15:09:15 -0500

    ✅ Validación de JWT implementada

--- src/main/java/com/pizzability/maker/web/config/JwtUtil.java
+++ src/main/java/com/pizzability/maker/web/config/JwtUtil.java
@@ -3,12 +3,15 @@ package com.pizzability.maker.web.config;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 @Component
+@Slf4j
 public class JwtUtil {
     private static final String SECRET_KEY = "P|zz4b1lit7/M@k3r";
     private static final Algorithm ALGORITHM = Algorithm.HMAC256(SECRET_KEY);
@@ -20,5 +23,24 @@ public class JwtUtil {
                 .withIssuedAt(new Date())
                 .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(14)))
                 .sign(ALGORITHM);
+    }
+
+    public boolean isValid(String jwt) {
+        try {
+            JWT.require(ALGORITHM)
+                    .build()
+                    .verify(jwt);
+            return true;
+        } catch (JWTVerificationException e) {
+            LOGGER.error(e.getMessage());
+            return false;
+        }
+    }
+
+    public String getUsername(String jwt) {
+        return JWT.require(ALGORITHM)
+                .build()
+                .verify(jwt)
+                .getSubject();
     }
 }
