c5af8e425ebc509f3133a68112ea10b12e77bea4
DavidGMont <davidgarmo@outlook.com>
2023-10-08 14:26:40 -0500

    🎫 Endpoint de inicio de sesión y retorno de JWT implementado

---
+++ src/main/java/com/pizzability/maker/service/dto/LoginDto.java
@@ -0,0 +1,9 @@
+package com.pizzability.maker.service.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/pizzability/maker/web/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.pizzability.maker.web.controller;
+
+import com.pizzability.maker.service.dto.LoginDto;
+import com.pizzability.maker.web.config.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@Slf4j
+public class AuthController {
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
+    @Autowired
+    public AuthController(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(@RequestBody LoginDto loginDto) {
+        UsernamePasswordAuthenticationToken login = new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
+        Authentication authentication = this.authenticationManager.authenticate(login);
+
+        LOGGER.warn("User: {}, Authenticated: {}", authentication.getPrincipal(), authentication.isAuthenticated());
+
+        String jwt = this.jwtUtil.create(loginDto.getUsername());
+        return ResponseEntity.ok().header(HttpHeaders.AUTHORIZATION, jwt).build();
+    }
+}
