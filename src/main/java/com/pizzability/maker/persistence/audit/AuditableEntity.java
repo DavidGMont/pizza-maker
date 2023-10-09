ebbd210fd2557be147bc78bc629777c92f1ac17a
DavidGMont <davidgarmo@outlook.com>
2023-10-08 20:06:59 -0500

    ✍ Auditoría con nombre de usuario añadida

--- src/main/java/com/pizzability/maker/persistence/audit/AuditableEntity.java
+++ src/main/java/com/pizzability/maker/persistence/audit/AuditableEntity.java
@@ -2,7 +2,9 @@ package com.pizzability.maker.persistence.audit;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
@@ -16,4 +18,11 @@ public class AuditableEntity {
     @Column(name = "last_modified_date")
     @LastModifiedDate
     private LocalDateTime lastModifiedDate;
+    @Column(name = "created_by")
+    @CreatedBy
+    private String createdBy;
+
+    @Column(name = "last_modified_by")
+    @LastModifiedBy
+    private String lastModifiedBy;
 }
